156ba28a0ff87a885832c94f9864bb88bd375407
Brian Button <bbutton@agilestl.com>
2017-02-10 20:26:41 -0600

    Refactored a bit to put data allocation at the same place as data initialization

--- main.c
+++ main.c
@@ -6,7 +6,10 @@
 #define MAX_ARGS 15
 #define MAX_ARG_LENGTH 256
 
-void read_command_line(char * args[]) {
+char ** read_command_line() {
+  char ** args = malloc(sizeof(char*[15]));
+  for(int i = 0; i < 15; i++) args[i] = NULL;
+
   char * buf;
   size_t length;
   size_t line_length = getline(&buf, &length, stdin);
@@ -18,27 +21,33 @@ void read_command_line(char * args[]) {
   for(char * arg = strtok(NULL, " \n\t"), i = 1; arg != NULL; arg = strtok(NULL, " \n\t"), i++) {
     args[i] = strdup(arg);
   }
+
+  return args;
 }
 
-int cd(char * args[]) { return chdir(args[1]); }
+int cd(char ** args) { return chdir(args[1]); }
 
 char * pwd() {
   return getcwd(NULL, 0);
 }
 
-void free_data(char * args[]) { for(int i = 0; i < MAX_ARGS; i++) if(args[i] != NULL) free(args[i]); }
+void debug(char ** args) {
+  for(int i = 0; i < MAX_ARGS; i++) {
+    printf("%d: <%s>\n", i, args[i]);
+  }
+}
+
+void free_data(char ** args) {
+  for(int i = 0; i < MAX_ARGS; i++) if(args[i] != NULL) free(args[i]);
+  free(args);
+}
 
 int main(int argc, char** argv) {
   while(1) {
     printf("> ");
 
-    char * args[15];
-    for(int i = 0; i < 15; i++) args[i] = NULL;
-    
-    read_command_line(args);
+    char ** args = read_command_line();
 
-    printf("command is %s\n", args[0]);
-    
     int ret = 0;
     char * res = NULL;
     
@@ -49,6 +58,9 @@ int main(int argc, char** argv) {
       res = pwd();
       printf("%s\n", res);
       free(res);
+    }
+    else if(strncmp(args[0], "debug", strlen("debug")) == 0) {
+      debug(args);
     }
 
     if(ret != 0) perror("failed to run command");
